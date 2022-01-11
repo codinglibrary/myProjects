e4c74a3193ec366674cedf514eb94de093e2212b
青酒-华为 <hobart_lh@163.com>
2022-01-11 09:56:37 +0800

    财管参数加密函数

--- JavaProjects/src/dataStructure/Test.java
+++ JavaProjects/src/dataStructure/Test.java
@@ -3,9 +3,15 @@ package dataStructure;
 import dataStructure.Base;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Test {
+    List<String> list=new LinkedList<>();
+    Map<String,String> map=new Map
     public static void main(String[] args) {
         System.out.println(Arrays.toString(Base.generateRandomIntArray()));
     }
+
 }
