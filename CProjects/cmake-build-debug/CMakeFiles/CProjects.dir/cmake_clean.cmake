97f57ef73105ba1ef4d6fca45048d81131f84ada
青酒-华为 <hobart_lh@163.com>
2022-02-28 13:28:38 +0800

    创建CProjects

---
+++ CProjects/cmake-build-debug/CMakeFiles/CProjects.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CProjects.dir/main.c.obj"
+  "CProjects.exe"
+  "CProjects.exe.manifest"
+  "CProjects.pdb"
+  "libCProjects.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CProjects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
