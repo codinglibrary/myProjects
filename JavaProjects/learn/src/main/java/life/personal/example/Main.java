fabff3f1e8c3fc09c44ef7bef6a62bdb6802c8a4
青酒-华为 <hobart_lh@163.com>
2022-03-02 19:37:46 +0800

    学习

--- JavaProjects/learn/src/main/java/life/personal/example/Main.java
+++ JavaProjects/learn/src/main/java/life/personal/example/Main.java
@@ -1,29 +1,12 @@
 package main.java.life.personal.example;
 
-import java.util.Arrays;
-
-import static main.java.life.qingjiu.dataStructure.utilities.Base.*;
-
 /**
  * @author David
  */
 public class Main {
     public static void main(String[] args) {
-        int[] a=getRandomIntArr(100,1);
-        printArray(a);
-        a=getRandomIntArr(10);
-        printArray(a);
-        int[] b=getRandomIntArr(1);
-        printArray(b);
-        b=getRandomIntArr(100,1,5);
-        printArray(b);
-        b=getRandomIntArr(100,1,-2);
-        printArray(b);
-        b=getRandomIntArr(100,1,-1);
-        printArray(b);
-        b=getRandomIntArr(100,-9,2);
-        printArray(b);
-        b=getRandomIntArr(100,2,2);
-        printArray(b);
+        for(int i=0;i<10;i++){
+            System.out.println(IdGenerator.getInstance().getId());
+        }
     }
 }
